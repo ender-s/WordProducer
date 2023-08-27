7f553a7d29cb0c20e6f24ca7d8dc590b9fdd6a72
ender-s <79700256+ender-s@users.noreply.github.com>
2023-08-27 20:23:58 +0300

    Add Generator.java

---
+++ generator/Generator.java
@@ -0,0 +1,17 @@
+package generator;
+
+import java.util.List;
+
+public abstract class Generator
+{
+    protected final List<String> generationPool;
+
+    /**
+     * Constructor of Generator class
+     * @param generationPool a List of Strings that defines the symbols used for word generation.
+     */
+    public Generator(List<String> generationPool)
+    {
+        this.generationPool = generationPool;
+    }
+}
